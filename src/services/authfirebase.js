373530b9bc687b8acf989c1cac04c7076afaa25a
saulo leocadio <sauloleocadio@MacBook-Air-de-saulo.local>
2021-05-17 09:53:25 -0300

    Change readme.md

--- src/services/authfirebase.js
+++ src/services/authfirebase.js
@@ -5,9 +5,52 @@ const socialMediaAuth = (provider) => {
   .auth()
   .signInWithPopup(provider)
   .then((res) => {
+    const {
+      REACT_APP_APIFIREBASE_KEY,
+      REACT_APP_AUTHDOMAIN,
+      REACT_APP_PROJECTID,
+      REACT_APP_STORAGEBUCKET,
+      REACT_APP_SENDERID,
+      REACT_APP_APPID,
+      REACT_APP_MEASUREMENTID
+     } = process.env;
+
+
+
+     console.log(
+       REACT_APP_APIFIREBASE_KEY,
+       REACT_APP_AUTHDOMAIN,
+       REACT_APP_PROJECTID,
+       REACT_APP_STORAGEBUCKET,
+       REACT_APP_SENDERID,
+       REACT_APP_APPID,
+       REACT_APP_MEASUREMENTID
+      );
     return res.user;
+
   })
   .catch((error)=> {
+    const {
+      REACT_APP_APIFIREBASE_KEY,
+      REACT_APP_AUTHDOMAIN,
+      REACT_APP_PROJECTID,
+      REACT_APP_STORAGEBUCKET,
+      REACT_APP_SENDERID,
+      REACT_APP_APPID,
+      REACT_APP_MEASUREMENTID
+     } = process.env;
+
+
+
+     console.log(
+       REACT_APP_APIFIREBASE_KEY,
+       REACT_APP_AUTHDOMAIN,
+       REACT_APP_PROJECTID,
+       REACT_APP_STORAGEBUCKET,
+       REACT_APP_SENDERID,
+       REACT_APP_APPID,
+       REACT_APP_MEASUREMENTID
+      );
     return error;
   });
 };
